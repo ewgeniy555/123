3591bb5feb2ef02125de59ba2db7629e6b7774db
ewgeniy212 <ewgeniy212@gmail.com>
2023-02-17 18:16:44 +0300

    выполнил задачу получил второй разряд из 3-х знаков  и сделал провеку ввода на три знака

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        
+            Console.WriteLine("принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.");
+            static int razr(int a)
+            {
+                
+            
+            
+               a = a / 10; 
+               a = a % 10;
+                return  a;
+            }
+            int b=0,c;
+            
+            while(b<=99){
+            
+            if (b<99){
+                 Console.WriteLine("в ведите число 3-х значное чило");
+                 b =int.Parse (Console.ReadLine());
+                 
+                 }
+                  if (b<99){
+                 Console.WriteLine("вы в вели двух значное число");
+                 
+                 
+                 }
+             
+            }        
+            c=razr(b);
+            b=0;
+            Console.WriteLine("результат");
+            Console.WriteLine(c);
+
+            
+
+
+
+
+           
+        
+    }
+}
